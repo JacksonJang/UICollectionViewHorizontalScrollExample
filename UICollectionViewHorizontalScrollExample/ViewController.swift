696e564fae9709975d8589ca479b924bedccd7aa
장효원 <hyo961015@naver.com>
2021-12-19 08:58:13 +0900

    add Horizontal Scroll

--- UICollectionViewHorizontalScrollExample/ViewController.swift
+++ UICollectionViewHorizontalScrollExample/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var collectionView: UICollectionView!
+    
+    var data: [String] = ["test", "test2", "test3", "test4", "test5", "test6", "test7"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Set Delegate
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
+        
+        //Set property value
+        collectionView.isPagingEnabled = true
+        collectionView.alwaysBounceHorizontal = true
+        
+        //Set FlowLayout
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 100, height: 100)
+        
+        collectionView.collectionViewLayout = layout
     }
 
+}
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
+        
+        cell.backgroundColor = .red
+        
+        return cell
+    }
 }
 
